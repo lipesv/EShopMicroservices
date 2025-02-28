71732997ccf10a27661dce16691d7a6d8a1ce98e
Fellipe da Silva Vieira <lipesv@gmail.com>
2025-02-28 13:45:31 -0300

    Implementation of discount micro service using gRPC to estabilish communication between basket service and discount service.

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,29 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMigration();
+app.MapGrpcService<DiscountService>();
+
+// Adiciona o serviço de reflexão
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
