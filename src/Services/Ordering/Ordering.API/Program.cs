dcf314c11d3e25896f5fab1f22faf23abead112e
Fellipe da Silva Vieira <lipesv@gmail.com>
2025-03-05 14:18:53 -0300

    Added Ordering Microservice to solution holding all needed layers related to this micro service, such as Ordering.API, Ordering.Domain, Ordering.Infrastructure and Ordering.Application. Completed development of Odering.Domain Layer and Ordering.Infrastructure layers.

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,28 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add services to the container
+
+builder.Services.AddApplicationServices()
+                .AddInfrastructure(builder.Configuration)
+                .AddApiServices();
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record ProductId
+{
+    public Guid Value { get; }
+
+    private ProductId(Guid value) => Value = value;
+
+    public static ProductId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException("ProductId cannot be empty.", nameof(value));
+        }
+
+        return new ProductId(value);
+    }
+}
