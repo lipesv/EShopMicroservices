2620bd5c6b49efd2afdab678e11f50eb837690b5
Fellipe da Silva Vieira <lipesv@gmail.com>
2025-03-05 17:44:46 -0300

    EMS-183 - Develop Orders DeleteOrderCommand and Handler using IApplicationDbContext. EMS-198

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: Delete Order entity from command object
+
+        var orderId = OrderId.Of(command.OrderId);
+        var order = await dbContext.Orders.FindAsync(orderId, cancellationToken);
+
+        if (order == null)
+        {
+            throw new OrderNotFoundException(command.OrderId);
+        }
+
+        // save to database
+
+        dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        // return result
+
+        return new DeleteOrderResult(true);
+    }
+}
