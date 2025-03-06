e2acf8d394bf8778f8105c130185aeaf9ce787cf
Fellipe da Silva Vieira <lipesv@gmail.com>
2025-03-06 11:12:49 -0300

    EMS-183: Included a pagination class for querying orders and GetOrders method to apply pagination feature. EMS-196 EMS-197

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,32 @@
+﻿
+using BuildingBlocks.Pagination;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        // TODO: Get orders with pagination
+
+        var pageIndex = query.PaginatonRequest.PageIndex;
+        var pageSize = query.PaginatonRequest.PageSize;
+
+        var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+        // return result
+
+        var orders = await dbContext.Orders.Include(o => o.OrderItems)
+                                           .AsNoTracking()
+                                           .OrderBy(o => o.OrderName.Value)
+                                           .Skip(pageIndex * pageSize)
+                                           .Take(pageSize)
+                                           .ToListAsync(cancellationToken);
+
+        return new GetOrdersResult(new PaginatedResult<OrderDto>(pageIndex,
+                                                                 pageSize,
+                                                                 totalCount,
+                                                                 orders.ToOrderDtoList()));
+    }
+}
