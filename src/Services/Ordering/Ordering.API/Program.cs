46216bf71234abdac7d705a6226a718337808123
Fellipe da Silva Vieira <lipesv@gmail.com>
2025-03-10 16:36:15 -0300

    Applied changes on Ordering API for preventing opening browser while testing API, to prevent consume rabbitmq message for Order Fullfilment using FeatureManagement flag. Removed one of exception handling into OrderName Value Object.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -17,10 +17,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseApiServices();
-
-if (app.Environment.IsDevelopment())
-{
-    await app.InitialiseDatabaseAsync();
-}
+await app.InitialiseDatabaseAsync();
 
 app.Run();
