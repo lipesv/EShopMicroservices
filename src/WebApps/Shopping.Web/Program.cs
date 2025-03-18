595ded73a81e180ca56d94cf7caa351a26f4347c
Fellipe da Silva Vieira <lipesv@gmail.com>
2025-03-18 14:21:14 -0300

    EMS-233: Developed Index C# Page to Consume YarpApiGateway Endpoints with ICatalogService. Implemented AddToCart Submit Handle in Index CSHTML Page with IBasketService.
    EMS-243
    EMS-244

--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -10,6 +10,11 @@ builder.Services.AddRefitClient<ICatalogService>().ConfigureHttpClient(c =>
     c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
 });
 
+builder.Services.AddRefitClient<IBasketService>().ConfigureHttpClient(c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
